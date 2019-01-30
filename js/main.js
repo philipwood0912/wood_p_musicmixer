80bf934059d420508fc6b8f13e8cd8f74f5845f7
adam wood <a_wood60810@fanshaweonline.ca>
2019-01-30 01:38:11 -0500

    added js, modified css + html

--- js/main.js
+++ js/main.js
@@ -1,1 +1,29 @@
-
+(() => {
+    
+    const icons = ['iconOne', 'iconTwo', 'iconThree', 'iconFour'];
+    
+    let iconsBox = document.querySelector('.iconsCon'),
+        treeBox = document.querySelector('#treeCon'),
+        iconSelectors = document.querySelectorAll('#iconButtons img');
+    
+    function createIcons(pictureIndex) {
+        
+        icons.forEach((icon, index) => {
+            let newIcons = `<img id="icon${index}" class="iconsImages" src="images/${icon + pictureIndex}.svg" alt="icon thumbnail">`
+            
+            iconsCon.innerHTML += newIcons;
+        });
+        
+    }
+    
+    function resetIcons() {
+        iconsCon.innerHTML = "";
+        createIcons(this.dataset.iconref);
+    }
+    
+    iconSelectors.forEach(iconImage => iconImage.addEventListener('click', resetIcons));
+    
+    createIcons(0);
+    
+    
+})();
